9a25d0f9d479ed15d3d3129219f5e277d823f9da
sunilbudhathoki01 <ashishbudhathoki952@gmail.com>
2026-03-31 07:20:49 +0545

    react and tailwind setup done and some routes setup

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-500 ">This is login page</h1>
+    </div>
+  );
+};
+
+export default Login;
